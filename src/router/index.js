d1211d655656d749e1a5a8c5e9cfd97594d036fe
chapintula902 <bgue969@lasierra.edu>
2022-05-25 11:54:40 -0700

    init commit
    init commit

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import GetStarted from '../screens/GetStarted';
+
+const Stack = createNativeStackNavigator();
+
+const Router = () => {
+  return (
+    <Stack.Navigator>
+    <Stack.Screen name="GetStarted" component={GetStarted} 
+    options={{
+        headerShown: false
+    }}
+    />
+    
+  </Stack.Navigator>
+  )
+}
+
+export default Router;
+
+const styles = StyleSheet.create({})
---
+++ src/screens/GetStarted/index.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const GetStarted  = () => {
+  return (
+    <View>
+      <Text> we getting started my nigs </Text>
+    </View>
+  )
+}
+
+export default GetStarted;
+
+const styles = StyleSheet.create({})
